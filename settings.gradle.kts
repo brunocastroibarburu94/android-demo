ce1f473e24595ea7efde90324ad4cff38bec6175
brunocastroibarburu94 <57188560+brunocastroibarburu94@users.noreply.github.com>
2025-05-01 19:55:11 +0100

    Android studio navigation template

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "health_tracker_app"
+include(":app")
+ 
